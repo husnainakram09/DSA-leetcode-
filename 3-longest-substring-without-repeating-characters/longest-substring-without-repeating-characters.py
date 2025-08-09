9cc9693a00483d1b7146b597cb22c246cda439ca
Husnain Akram <57561973+husnainakram09@users.noreply.github.com>
2025-08-09 17:32:03 +0800

    Time: 13 ms (86.67%) | Memory: 17.9 MB (52.40%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,10 +3,10 @@ class Solution:
         dict = {}
         position = 0
         length = 0
-
         for i in range(len(s)):
             if s[i] in dict and dict[s[i]] >= position:
                 position = dict[s[i]] + 1
             dict[s[i]] = i
-            length = max(length, i - position +1)
+            length = max(length, i - position + 1)
+            
         return length
