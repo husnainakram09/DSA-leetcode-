83df1dfd592c840b0118c3e513ddce6512bbffb0
Husnain Akram <57561973+husnainakram09@users.noreply.github.com>
2025-08-10 17:46:33 +0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.05%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.py
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -1,7 +1,7 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         def digit_count(num):
-            count = [0] * 10  # index = digit
+            count = [0] * 10 
             while num > 0:
                 count[num % 10] += 1
                 num //= 10
@@ -9,7 +9,7 @@ class Solution:
         
         target_count = digit_count(n)
         
-        for i in range(31):  # 2^0 to 2^30
+        for i in range(31):
             if digit_count(1 << i) == target_count:
                 return True
         return False
